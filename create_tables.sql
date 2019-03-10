97fe8192d67f81e8d06e18a46fb492846a3dcb25
Clayton Snyder <clayton.snyder@ddswireless.com>
2019-03-10 01:09:29 -0800

    switch to handlebars, set up DB

---
+++ create_tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS pokemon
+(
+	id				INTEGER	PRIMARY KEY,
+    name			VARCHAR(20) NOT NULL UNIQUE,
+    typePrimary		TEXT NOT NULL,
+    typeSecondary	TEXT,
+    flavorText		TEXT
+);
+
+CREATE TABLE IF NOT EXISTS review
+(
+    id              INTEGER PRIMARY KEY,
+    pokemonPK       INTEGER NOT NULL,
+    stars           INTEGER NOT NULL,
+    reviewText      TEXT
+);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const _ = require('lodash');
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host      : process.env.RDS_HOST || 'localhost',
+  user      : 'yelpokemon',
+  password  : 'yelpokemon',
+  database  : 'yelpokemon'
+});
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to database.');
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'YELPOKEMON' });
+router.get('/', (req, res) => {
+  let result = '';
+  db.query('SELECT * FROM pokemon', (err, rows) => {
+    if (err) throw err;
+    _.forEach(rows, (row) => {
+      let typeSecondary = (row.typeSecondary.length > 0 ? `, ${row.typeSecondary}` : '');
+      console.log(`#${row.id}: ${row.name} [${row.typePrimary}${typeSecondary}] ${row.flavorText}`);
+    });
+    for (let i = 0; i < rows.length; i++) {
+      result += `${rows[i].id}:${rows[i].name}    `;
+    }
+    console.log('after loading: ' + result);
+    res.render('index', { title: 'YELPOKEMON', result: result });
+  });
+});
+
+router.get('/about', (req, res) => {
+  res.send('abouut');
 });
 
 module.exports = router;
