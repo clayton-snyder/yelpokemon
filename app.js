843fa2bea0557ee626170adaf55e678c21eb0326
cat <catsan@uw.edu>
2019-03-11 22:41:04 -0700

    added front end nav bar

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
@@ -23,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
+
+// maintains link to stylesheet
+app.use("/public",express.static(__dirname + "/public"));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
